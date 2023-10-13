41da55c0fd3cd277869fc1d67cd50dafe4bbe62d
Felix Jung <felix.j.jung@gmail.com>
2023-10-14 00:57:49 +0200

    Refactor QuickMateBot for efficiency and added new features
    Refactored QuickMateBot in Chess-Challenge program to improve its performance and make it more robust. Added new features like providing raw count of all nodes, added a dictionary for tracking each move count, integrated a function to order moves to improve decision making, added a new evaluation function to return score for a particular configuration, and added new scoring method for moves. Refinements also included changing variables to private and readonly members for the code integrity and efficiency. These changes have significantly improved the performance and decision making of QuickMateBot.

--- Chess-Challenge/src/My Bot/QuickMateBot.cs
+++ Chess-Challenge/src/My Bot/QuickMateBot.cs
@@ -1,153 +1,198 @@
-﻿using System;
+﻿#region
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using ChessChallenge.API;
 
-namespace Chess_Challenge.My_Bot {
-    public class QuickMateBot : IChessBot {
-        public int MaxDepth = 5;
-        private Stopwatch stopwatch = new Stopwatch();
-        public long PositionsEvaluated { get; private set; }
-        public int BestScore { get; private set; }
+#endregion
 
-        public Move Think(Board board, Timer timer) {
-            PositionsEvaluated = 0;
-            var legalMoves = board.GetLegalMoves();
-            // legalMoves = legalMoves.OrderBy(_ => Guid.NewGuid()).ToArray();
+namespace Chess_Challenge.My_Bot;
 
-            var bestMove = Move.NullMove;
-            int bestScore = int.MinValue;
+public class QuickMateBot : IChessBot {
+    private readonly bool _printNodeCount = false;
+    private readonly bool _printMoves = false;
+        private const int MAX_PIECE_SCORE = 100000;
+    private readonly Stopwatch _stopwatch = new();
+        private int _localNodeCount;
+        private int _pruningCount;
 
-            stopwatch.Restart();
+        public int MaxDepth { get; set; } = 4;
+        public Dictionary<Move, int> MoveCounts { get; set; } = new();
 
-            foreach (var move in legalMoves) {
-                // Mate in one
-                board.MakeMove(move);
+    public int NodeCount { get; private set; }
+    public int BestScore => 0;
 
-                if (board.IsInCheckmate()) {
-                    Console.WriteLine("Raw  Checkmate in 1");
-                    board.UndoMove(move);
-                    return move;
-                }
+    public Move Think(Board board, Timer timer) {
+        var legalMoves = board.GetLegalMoves();
+        legalMoves = legalMoves.OrderBy(_ => Guid.NewGuid()).ToArray();
 
-                int score = -NegaMax(board, MaxDepth, int.MinValue, int.MaxValue);
-                board.UndoMove(move);
+        var bestMove = Move.NullMove;
+        int bestScore = int.MinValue;
 
-                if (score > bestScore) {
-                    bestScore = score;
-                    bestMove = move;
-                }
-                Console.WriteLine($"Move: {move}, Score: {score}");
-            }
+        _stopwatch.Restart();
 
-            stopwatch.Stop();
-            Console.WriteLine(
-                    $"Depth: {MaxDepth}, Time: {stopwatch.ElapsedMilliseconds} ms, Score: {bestScore}, Best Move:{bestMove} Positions: {PositionsEvaluated}");
-            return bestMove;
-        }
-        private int AssessBoard(Board board,int depth) {
-            int score = 0;
-            var allPieces = board.GetAllPieceLists();
-            int who2Move;
-            if (board.IsWhiteToMove) {
-                 who2Move = 1;
-            } else {
-                 who2Move = -1;
-            }
-
-            // if checkmate, return max score
-            if (board.IsInCheckmate()) {
-                return who2Move * (100000 + depth);
-              
-            }
-
-            foreach (var pieceList in allPieces) {
-                foreach (var piece in pieceList) {
-                    int pieceScore = GetScore(piece.PieceType);
-
-                    if (piece.IsWhite == board.IsWhiteToMove) {
-                        score += pieceScore;
-                    } else {
-                        score -= pieceScore;
-                    }
-                }
-            }
-
-            return score;
-        }
-
-        private int GetScore(PieceType pieceType) {
-            return pieceType switch {
-                PieceType.Pawn => 100,
-                PieceType.Knight => 300,
-                PieceType.Bishop => 300,
-                PieceType.Rook => 500,
-                PieceType.Queen => 900,
-                PieceType.King => int.MaxValue / 2,
-                _ => 0,
-            };
-        }
-
-        private int NegaMax(Board board, int depth, int alpha, int beta) {
-            if (depth == 0 || board.IsInCheckmate() || board.IsInStalemate()) {
-                if (board.IsInCheckmate()) {
-
-
-                }
-
-                PositionsEvaluated++;
-                return AssessBoard(board,depth);
-            }
-
-            var moves = board.GetLegalMoves();
-            // Console.WriteLine("Moves: {moves}");
-
-            moves = OrderMoves(moves, board);
-            // Console.WriteLine($"Ordered moves: {moves}");
-
-            int score = int.MinValue;
-
-            foreach (var move in moves) {
-                board.MakeMove(move);
-                score = Math.Max(score, -NegaMax(board, depth - 1, -beta, -alpha));
-                board.UndoMove(move);
-
-                alpha = Math.Max(alpha, score);
-
-                if (alpha >= beta) {
-                    break;
-                }
-            }
-
-            return score;
-        }
-        private static readonly Random
-                rng = new Random(); // used to randomly order moves of equal value
-
-        private Move[] OrderMoves(IEnumerable<Move> moves, Board board) {
-            var movesWithScore = moves.Select(move => new {move, score = ScoreMove(move, board)});
-            var orderedMoves = movesWithScore.OrderByDescending(x => x.score).Select(x => x.move)
-                    .ToArray();
-            return orderedMoves;
-        }
-
-        private int ScoreMove(Move move, Board board) {
+        foreach (var move in legalMoves) {
+            _localNodeCount = 0;
+            // Mate in one
             board.MakeMove(move);
-
-            var score = 0;
-
-            if (board.IsInCheck()) {
-                score += 10000;
-            } else if (move.IsCapture != null) {
-                score += 2000;
+            if (board.IsInCheckmate()) {
+                Console.WriteLine("Raw  Checkmate in 1");
+                board.UndoMove(move);
+                return move;
             }
 
-            // score += rng.Next(10);
 
+            int score = -NegaMax(board, MaxDepth-1, int.MinValue, int.MaxValue);
+            board.UndoMove(move);
+            // nodeCount += localnodes;
+
+            if (!MoveCounts.ContainsKey(move)) {
+                MoveCounts.Add(move, _localNodeCount);
+                NodeCount += _localNodeCount;
+            }
+
+            if (score > bestScore)
+            {
+                bestScore = score;
+                bestMove = move;
+            }
+
+            if (_printMoves) {
+                var replace = move.ToString().Replace("\'", "").Replace("Move: ", "");
+                Console.WriteLine($"Move: {replace}, Score: {score}");
+            }
+        }
+
+        _stopwatch.Stop();
+        if (_printNodeCount) PrintNodes();
+
+        Console.WriteLine(
+                $"QuickMateBot:  Depth: {MaxDepth}, Time: {_stopwatch.ElapsedMilliseconds} ms, Score: {bestScore}, Best Move:{bestMove} Nodes: {NodeCount}");
+        return bestMove;
+    }
+    private int Evaluate(Board board, int depth) {
+        int score = 0;
+        var allPieces = board.GetAllPieceLists();
+        int who2Move;
+        if (board.IsWhiteToMove) {
+            who2Move = 1;
+        } else {
+            who2Move = -1;
+        }
+
+        if (board.IsInCheckmate()) {
+            return who2Move * (MAX_PIECE_SCORE + depth);
+              
+        }
+
+        foreach (var pieceList in allPieces) {
+            foreach (var piece in pieceList) {
+                int pieceScore = GetScore(piece.PieceType);
+
+                if (piece.IsWhite == board.IsWhiteToMove) {
+                    score += pieceScore;
+                } else {
+                    score -= pieceScore;
+                }
+            }
+        }
+
+        return score;
+    }
+
+    private int GetScore(PieceType pieceType) {
+        return pieceType switch {
+            PieceType.Pawn => 100,
+            PieceType.Knight => 300,
+            PieceType.Bishop => 300,
+            PieceType.Rook => 500,
+            PieceType.Queen => 900,
+            PieceType.King => int.MaxValue / 2,
+            _ => 0,
+        };
+    }
+
+    private int NegaMax(Board board, int depth, int alpha, int beta) {
+        if (depth == 0 || board.IsInCheckmate() || board.IsInStalemate()) {
+            if (board.IsInCheckmate()) {
+
+
+            }
+            _localNodeCount++;
+            return Evaluate(board, depth);
+        }
+
+
+        int score = int.MinValue;
+        var moves = board.GetLegalMoves();
+        moves = OrderMoves(moves, board);
+
+        int moveCount = moves.Length;
+        int processedMoves = 0;
+        // nodeCount += moves.Length;
+        // moves = OrderMoves(moves, board);
+
+        foreach (var move in moves) {
+            board.MakeMove(move);
+            processedMoves++;
+
+            score = Math.Max(score, -NegaMax(board, depth - 1, -beta, -alpha));
             board.UndoMove(move);
 
-            return score;
+
+            alpha = Math.Max(alpha, score);
+
+            if (alpha >= beta) {
+                _pruningCount += (moveCount - processedMoves);
+                break;
+            }
         }
+
+        return score;
+    }
+
+    private Move[] OrderMoves(IEnumerable<Move> moves, Board board) {
+        var movesWithScore = moves.Select(move => new {move, score = ScoreMove(move, board)});
+        var orderedMoves = movesWithScore.OrderByDescending(x => x.score).Select(x => x.move)
+                .ToArray();
+        return orderedMoves;
+    }
+    private void PrintNodes() {
+        Console.WriteLine($"Total: {NodeCount}");
+
+        foreach (var move in MoveCounts.OrderBy(m => m.Key.ToString())) {
+            Console.WriteLine(
+                    $"{move.Key.ToString().Replace("\'", "").Replace("Move: ", "")} - {move.Value}");
+        }
+
+        Console.WriteLine($"Pruned nodes count: {_pruningCount}");
+
+        double percentageSaved = (double)_pruningCount / NodeCount * 100;
+        Console.WriteLine($"Percentage saved: {percentageSaved:0.00}% of {NodeCount} nodes");
+    }
+
+    private int ScoreMove(Move move, Board board) {
+        board.MakeMove(move);
+        bool isInCheck = board.IsInCheck();
+        bool isInCheckmate = board.IsInCheckmate();
+
+        int score = 0;
+
+        if (move.IsCapture) {
+            score += GetScore(move.CapturePieceType)* 100;
+        }
+
+        if (isInCheck) {
+            score += 10000;
+        }
+
+        if (isInCheckmate) {
+            score += 100000;
+        }
+        board.UndoMove(move);
+
+        return score;
     }
 }
