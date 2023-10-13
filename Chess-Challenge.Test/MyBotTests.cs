00d149edaf9545f3d20228cd672ba8fa52d55d6e
Felix Jung <felix.j.jung@gmail.com>
2023-10-13 05:08:22 +0200

    really slow

--- Chess-Challenge.Test/MyBotTests.cs
+++ Chess-Challenge.Test/MyBotTests.cs
@@ -1,18 +1,37 @@
+#region
+
 using ChessChallenge.API;
 using Timer = ChessChallenge.API.Timer;
+
+#endregion
 
 namespace Chess_Challenge.Test;
 
 public class MyBotTests {
     [Fact]
-    public void TestCheckmateInTwo()
-    {
+    public void TestCaptureHighestValuePiece() {
+        // Arrange
+        var board = Board.CreateBoardFromFEN("5k2/8/8/4q3/8/3N4/PPP2r2/1KR5 w - - 0 1");
+        var timer = new Timer(60 * 1000);
+        var bot = new MyBot {
+            MaxDepth = 3,
+        };
+
+        // Act
+        var move = bot.Think(board, timer);
+
+        // Assert
+        var expectedMove = new Move("d3e5", board);
+        Assert.Equal(expectedMove, move);
+    }
+    [Fact]
+    public void TestCheckmateInTwo() {
         // Arrange
         var board = Board.CreateBoardFromFEN("8/3pR3/N1Nk4/1p3Pp1/5q2/6B1/1K6/7Q w - - 0 1");
         var timer = new Timer(60 * 1000);
-        var bot = new MyBot();
-
-        bot.maxDepth = 3;
+        var bot = new MyBot {
+            MaxDepth = 3,
+        };
 
         // Act
         var move = bot.Think(board, timer);
@@ -22,50 +41,14 @@ public class MyBotTests {
         Assert.Equal(expectedMove, move);
     }
 
-
     [Fact]
-    public void TestStartFenScoreZero() {
-        Board board =
-                Board.CreateBoardFromFEN(
-                        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-        Timer timer = new Timer(60 * 1000);
-        var bot = new MyBot();
-
-        bot.maxDepth = 3;
-
-        // Act
-        var move = bot.Think(board, timer);
-
-        // Assert
-        var ExpectedScore = 0;
-        Assert.Equal(ExpectedScore, bot.BestScore);
-    }
-
-    [Fact]
-    public void TestCaptureHighestValuePiece()
-    {
-        // Arrange
-        var board = Board.CreateBoardFromFEN("5k2/8/8/4q3/8/3N4/PPP2r2/1KR5 w - - 0 1");
-        var timer = new Timer(60 * 1000);
-        var bot = new MyBot();
-        bot.maxDepth = 3;
-
-        // Act
-        var move = bot.Think(board, timer);
-
-        // Assert
-        var expectedMove = new Move("d3e5", board);
-        Assert.Equal(expectedMove, move);
-    }
-
-    [Fact]
-    public void TestForcedSequenceToWinQueen()
-    {
+    public void TestForcedSequenceToWinQueen() {
         // Arrange
         var board = Board.CreateBoardFromFEN("r7/5k1p/pppp1NpQ/4p2n/3P4/2P3Pq/Pr6/5RK1 w - - 3 27");
         var timer = new Timer(60 * 1000);
-        var bot = new MyBot();
-        bot.maxDepth = 5;
+        var bot = new MyBot {
+            MaxDepth = 5,
+        };
 
         // Act
         var move = bot.Think(board, timer);
@@ -73,5 +56,24 @@ public class MyBotTests {
         // Assert
         var expectedMove = new Move("h6h7", board);
         Assert.Equal(expectedMove, move);
+    }
+
+
+    [Fact]
+    public void TestStartFenScoreZero() {
+        var board =
+                Board.CreateBoardFromFEN(
+                        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+        var timer = new Timer(60 * 1000);
+        var bot = new MyBot {
+            MaxDepth = 3,
+        };
+
+        // Act
+        bot.Think(board, timer);
+
+        // Assert
+        int expectedScore = 0;
+        Assert.Equal(expectedScore, bot.BestScore);
     }
 }
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,153 +1,62 @@
-﻿using System;
+﻿#region
+
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using ChessChallenge.API;
 
+#endregion
+
 
 public class MyBot : IChessBot {
-    public int maxDepth = 5;
+    public int MaxDepth = 5;
+    private Stopwatch stopwatch = new Stopwatch();
+    public long PositionsEvaluated { get; private set; }
     public int BestScore { get; private set; }
-    private Stopwatch stopwatch;
-    private double timeToUse;
 
-    public Move Think(Board board, Timer timer)
-    {
+    public Move Think(Board board, Timer timer) {
+        PositionsEvaluated = 0;
         var legalMoves = board.GetLegalMoves();
-        legalMoves = legalMoves.OrderBy(x => Guid.NewGuid()).ToArray();
+        // legalMoves = legalMoves.OrderBy(_ => Guid.NewGuid()).ToArray();
 
-        Move bestMove = Move.NullMove;
-        int bestScore = Int32.MinValue;
+        var bestMove = Move.NullMove;
+        int bestScore = int.MinValue;
 
-        // ############ Time management ############
-        double moveOverhead = 10; // Adjust as needed
-        double slowMover = 1;     // Adjust as needed
-
-        double timeLeft = Math.Max(0, timer.MillisecondsRemaining - moveOverhead);
-        double gameLength;
-
-        if (board.PlyCount < 40) gameLength = 112.0; // Opening game
-        else gameLength = 176.0;                     // Middle/endgame - This is a simplification
-
-        stopwatch = Stopwatch.StartNew();
-        timeToUse = timeLeft * slowMover *15 /
-                    ((gameLength - board.PlyCount) * slowMover +7.5 - board.PlyCount);
-        // #########################################
-        int depth = 1;
-        // depending on how many ply we have left, we can increase the depth
-        // print time to use in seconds
-        Console.WriteLine($"Time to use: {timeToUse / 1000} seconds");
-
-        Move lastCompletedDepthBestMove = Move.NullMove;
-
-        //while (stopwatch.ElapsedMilliseconds < timeToUse && depth <= maxDepth) {
-
-        foreach (var move in legalMoves)
-        {
+        stopwatch.Restart();
+        foreach (var move in legalMoves) {
             // Mate in one
             board.MakeMove(move);
 
-            if (board.IsInCheckmate())
-            {
+            if (board.IsInCheckmate()) {
                 board.UndoMove(move);
                 return move;
             }
 
-            var score = Minimax(board, maxDepth, Int32.MinValue, Int32.MaxValue, false);
+            int score = Minimax(board, MaxDepth, int.MinValue, int.MaxValue, false);
             board.UndoMove(move);
 
-            if (score > bestScore)
-            {
+            if (score > bestScore) {
                 bestScore = score;
                 bestMove = move;
             }
         }
-        /*
-         if (stopwatch.ElapsedMilliseconds < timeToUse) {
-            lastCompletedDepthBestMove = bestMove;
-        }
 
-
-        Console.WriteLine(
-                $"Depth: {depth}, Time taken: {stopwatch.Elapsed} seconds, Best move: {bestMove}");
-        depth++;
-    } */
-
-        Console.WriteLine($"Best move: {bestMove} with score {bestScore}");
-        BestScore = bestScore;
+        stopwatch.Stop();
+        Console.WriteLine($"Depth: {MaxDepth}, Time: {stopwatch.ElapsedMilliseconds} ms, Score: {bestScore}, Positions: {PositionsEvaluated}");
         return bestMove;
-        // return lastCompletedDepthBestMove;
     }
-
-    private int Minimax(Board board, int depth, int alpha, int beta, bool maximizingPlayer)
-    {
-        if (depth == 0)
-        {
-            return AssessBoard(board);
-        }
-
-        Move[] moves = board.GetLegalMoves();
-        // moves = moves.OrderBy(x => Guid.NewGuid()).ToArray();
-
-        if (maximizingPlayer)
-        {
-            var maxEvaluation = Int32.MinValue;
-
-            foreach (var move in moves)
-            {
-                board.MakeMove(move);
-                var evaluation = Minimax(board, depth - 1, alpha, beta, false);
-                board.UndoMove(move);
-
-                maxEvaluation = Math.Max(maxEvaluation, evaluation);
-                alpha = Math.Max(alpha, evaluation);
-
-                if (alpha >= beta)
-                {
-                    break;
-                }
-            }
-
-            return maxEvaluation;
-        }
-        else
-        {
-            var minEvaluation = Int32.MaxValue;
-
-            foreach (var move in moves)
-            {
-                board.MakeMove(move);
-                var evaluation = Minimax(board, depth - 1, alpha, beta, true);
-                board.UndoMove(move);
-
-                minEvaluation = Math.Min(minEvaluation, evaluation);
-                beta = Math.Min(beta, evaluation);
-
-                if (alpha >= beta)
-                {
-                    break;
-                }
-            }
-
-            return minEvaluation;
-        }
-    }
-
-    private int AssessBoard(Board board)
-    {
+    private int AssessBoard(Board board) {
         int score = 0;
         var allPieces = board.GetAllPieceLists();
 
-        foreach (var pieceList in allPieces)
-        {
-            foreach (var piece in pieceList)
-            {
-                var pieceScore = GetScore(piece.PieceType);
-                if(piece.IsWhite == board.IsWhiteToMove)
-                {
+        foreach (var pieceList in allPieces) {
+            foreach (var piece in pieceList) {
+                int pieceScore = GetScore(piece.PieceType);
+
+                if (piece.IsWhite == board.IsWhiteToMove) {
                     score += pieceScore;
-                }
-                else
-                {
+                } else {
                     score -= pieceScore;
                 }
             }
@@ -156,10 +65,8 @@ public class MyBot : IChessBot {
         return score;
     }
 
-    private int GetScore(PieceType pieceType)
-    {
-        return pieceType switch
-        {
+    private int GetScore(PieceType pieceType) {
+        return pieceType switch {
             PieceType.Pawn => 100,
             PieceType.Knight => 300,
             PieceType.Bishop => 300,
@@ -168,5 +75,80 @@ public class MyBot : IChessBot {
             PieceType.King => 10000, // Some large number
             _ => 0,
         };
+    }
+
+    private int Minimax(Board board, int depth, int alpha, int beta, bool maximizingPlayer) {
+        if (depth == 0) {
+            PositionsEvaluated++;
+            return AssessBoard(board);
+        }
+
+        var moves = board.GetLegalMoves();
+        // Console.WriteLine("Moves: {moves}");
+
+        // moves = OrderMoves(moves, board);
+        // Console.WriteLine($"Ordered moves: {moves}");
+
+        if (maximizingPlayer) {
+            int maxEvaluation = int.MinValue;
+
+            foreach (var move in moves) {
+                board.MakeMove(move);
+                int evaluation = Minimax(board, depth - 1, alpha, beta, false);
+                board.UndoMove(move);
+
+                maxEvaluation = Math.Max(maxEvaluation, evaluation);
+                alpha = Math.Max(alpha, evaluation);
+
+                if (alpha >= beta) {
+                    break;
+                }
+            }
+
+            return maxEvaluation;
+        }
+
+        int minEvaluation = int.MaxValue;
+
+        foreach (var move in moves) {
+            board.MakeMove(move);
+            int evaluation = Minimax(board, depth - 1, alpha, beta, true);
+            board.UndoMove(move);
+
+            minEvaluation = Math.Min(minEvaluation, evaluation);
+            beta = Math.Min(beta, evaluation);
+
+            if (alpha >= beta) {
+                break;
+            }
+        }
+
+        return minEvaluation;
+    }
+    private static readonly Random rng = new Random(); // used to randomly order moves of equal value
+
+    private Move[] OrderMoves(IEnumerable<Move> moves, Board board) {
+        var movesWithScore = moves.Select(move => new {move, score = ScoreMove(move, board)});
+        var orderedMoves = movesWithScore.OrderByDescending(x => x.score).Select(x => x.move)
+                .ToArray();
+        return orderedMoves;
+    }
+
+    private int ScoreMove(Move move, Board board) {
+        board.MakeMove(move);
+
+        var score = 0;
+
+        if (board.IsInCheck()) {
+            score += 10000;
+        } else if (move.IsCapture != null) {
+            score += 2000;
+        }
+
+        score += rng.Next(10);
+
+        board.UndoMove(move);
+
+        return score;
     }
 }
